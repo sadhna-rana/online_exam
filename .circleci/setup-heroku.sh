86895d382ca985b6ca4310a29a34f560595259cf
sadhnarana17 <sadhnarana@ucreate.co.in>
2018-05-03 12:45:59 +0530

    added circle ci integration

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,4 +1,5 @@
 git remote add heroku https://git.heroku.com/online-exam-stagging.git
+
 wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
 sudo mkdir -p /usr/local/lib /usr/local/bin
    sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
@@ -12,5 +13,7 @@ sudo mkdir -p /usr/local/lib /usr/local/bin
       login $HEROKU_LOGIN
       password $HEROKU_API_KEY
     EOF
+
     # Add heroku.com to the list of known hosts
     ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
+    
